883cf68e11eb22488cf8b3698f032baa7e69e427
Gabriel Peart <gabriel.peart@me.com>
2016-03-11 08:32:06 +0100

    adjusting to follow the same syntax

--- src/math/Constants.js
+++ src/math/Constants.js
@@ -1,4 +1,4 @@
 export const PI2 = Math.PI * 2;
 export const HALFPI = Math.PI * 0.5;
 export const EPSILON = 1.0e-6;
-export const PHI = 1.618033988749895;
+export const PHI = (1 + Math.sqrt(5)) * 0.5;
